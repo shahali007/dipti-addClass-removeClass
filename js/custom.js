b951d85d07caf857299ab7cc16f95359e9a9992a
ShahAli bogdadi <bogdadishahali@gmail.com>
2016-08-08 15:32:55 +0600

    dipti-addClass-removeClass
    addClass-removeClass

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+// accordian
+	$('.accordion-toggle').on('click', function(){
+		$(this).closest('.panel-group').children().each(function(){
+		$(this).find('>.panel-heading').removeClass('active');
+		 });
+
+	 	$(this).closest('.panel-heading').toggleClass('active');
+	});
